f4003270485f42f0d3ea7b734c49883402923abc
Shalauddin Ahamad Shuza <shalauddin.shuza@bs-23.net>
2018-03-20 01:35:15 +0600

    Annotations are added

---
+++ app/src/main/java/me/shuza/android_mvp/annotations/PerFragment.kt
@@ -0,0 +1,20 @@
+package me.shuza.android_mvp.annotations
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import javax.inject.Scope
+
+/**
+ *
+ * :=  created by:  Shuza
+ * :=  create date:  3/20/2018
+ * :=  (C) CopyRight Shuza
+ * :=  www.shuza.me
+ * :=  shuza.sa@gmail.com
+ * :=  Fun  :  Coffee  :  Code
+ *
+ **/
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+annotation class PerFragment
