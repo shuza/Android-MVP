68ffeb80fe4f3675db95b4bb135e78752e5c9aee
Shalauddin Ahamad Shuza <shuza.sa@gmail.com>
2018-07-03 08:15:07 +0600

    Update BaseActivity.kt

--- app/src/main/java/me/shuza/android_mvp/base/BaseActivity.kt
+++ app/src/main/java/me/shuza/android_mvp/base/BaseActivity.kt
@@ -21,7 +21,7 @@ import kotlin.properties.Delegates
  * :=  created by:  Shuza
  * :=  create date:  3/22/2018
  * :=  (C) CopyRight Shuza
- * :=  www.shuza.me
+ * :=  www.shuza.ninja
  * :=  shuza.sa@gmail.com
  * :=  Fun  :  Coffee  :  Code
  *
